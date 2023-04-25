23a8f4a702fc865566dd188011db99a34baa5083
RaviAtal <raviatal44@gmail.com>
2023-04-25 13:39:09 +0530

    final

--- src/Components/PreLoader.jsx
+++ src/Components/PreLoader.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import logo from "../Assets/Images/png/solana_nav_logo.png";
 
 const PreLoader = () => {
   const [loader, setLoader] = useState(true);
@@ -8,14 +9,20 @@ const PreLoader = () => {
       setLoader(false);
       document.body.style.display = "block";
       document.body.classList.remove("overflow-hidden");
-    }, 3000);
+    }, 5000);
 
     // clean up the timer on unmount
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <>{loader ? <div className="preloader ">Loading...</div> : null}</>
+    <>
+      {loader ? (
+        <div className="preloader ">
+          <img className="start" src={logo} alt="logo" />
+        </div>
+      ) : null}
+    </>
   );
 };
 
